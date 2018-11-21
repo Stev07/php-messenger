003fd8b8a860f598f59e7844df3625dee4240cc7
Valentin Grégoire <valentin.gregoire91@gmail.com>
2018-11-21 16:55:09 +0100

    messages added

--- src/views/message.php
+++ src/views/message.php
@@ -6,25 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/css/styles.css" />
+    <link rel="stylesheet" href="../public/css/styles.css" />
 
     <title>PH-Essenger</title>
 </head>
-<body>
-    <main class="container-fluid">
-        <!-- Section contenant les topics -->
-        <section>
+<body class="container-fluid">
+    <main class="row">
+        <!-- Section with all the topics created -->
+        <section id="topics" class="col-3">
 
         </section>
 
-        <!-- Section contenant les messages -->
-        <section>
-
-        </section>
-
-        <!-- Section contenant les personnes inscrites -->
-        <section>
+        <!-- Section which display all the messages from a topic -->
+        <section id="messages" class="col-6">
             
+
+            <form id="message" action="" method="post" class="row">
+                <input type="text" name="message" placeholder="Your message here" class="form-control col-11"/>
+                <button type="submit" name="send" class="img-fluid col-1"><img src="../public/images/arrow.svg" class="img-fluid send-image"></button>
+            </form>
+        </section>
+
+        <!-- Section which display all the subscribers -->
+        <section id="subs" class="col-3">
+
         </section>
     </main>
 </body>
