83d863e53cd12ac88241043dac875855893cf161
Valentin Grégoire <valentin.gregoire91@gmail.com>
2018-11-21 14:17:10 +0100

    Styles added

--- src/index.php
+++ src/index.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link type="text/stylesheet" rel="stylesheet" href="src/public/css/bootstrap.min.css" />
+    <link type="text/stylesheet" rel="stylesheet" href="src/public/css/styles.css" />
     <title>Document</title>
 </head>
 <body>
-    
+    <main class="container-fluid">
+
+    </main>
+
     <script src="src/public/js/jquery.min.js"></script>
     <script src="src/public/js/bootstrap.min.js"></script>
 </body>
